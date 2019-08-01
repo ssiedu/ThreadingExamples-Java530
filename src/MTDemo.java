511214368a35a18b6b3f81c2235696c85cc9685b
manoj <manoj.sarwate@ssiedu.in>
2019-08-01 18:31:19 +0530

    01-08-19

--- src/MTDemo.java
+++ src/MTDemo.java
@@ -1,30 +1,38 @@
+import java.util.Scanner;
 public class MTDemo extends Thread {
     
     public void run(){
-        disp();
+	String s=getName();//running thread
+      	System.out.println(s+"=>Statement-1");
+	if(s.equals("BBB"))
+	{
+		System.out.println(10/0);
+		//stop();
+		try{
+		//Thread.sleep(25000);//milli-seconds
+		}catch(Exception e){}
+		//Scanner sc=new Scanner(System.in);
+		//System.out.println("Enter Some Data For BBB");
+		//int data=sc.nextInt();
+	}
+	System.out.println(s+"=>Statement-2");
+        	System.out.println(s+"=>Statement-3");
+        	System.out.println(s+"=>Statement-4");
     }
-    public void disp(){
-        System.out.println("Statement-1");
-        System.out.println("Statement-2");
-        System.out.println("Statement-3");
-        System.out.println("Statement-4");
-    }
-    public static void main(String args[]){
-
-        MTDemo ob1=new MTDemo();    //first thread
-        MTDemo ob2=new MTDemo();    //second thread
-        MTDemo ob3=new MTDemo();    //third thread
-        ob1.start();    //started first thread
-        ob2.start();    //started second thread
-        ob3.start();    //started third thread
-        
-        
+    public static void main(String args[]) throws Exception{
+	System.out.println("Main-Method-Started");
+	MTDemo t1=new MTDemo(); t1.setName("AAA");
+	MTDemo t2=new MTDemo(); t2.setName("BBB");
+	MTDemo t3=new MTDemo(); t3.setName("CCC");
+	MTDemo t4=new MTDemo(); t4.setName("DDD");
+	t1.start();
+	t2.start();
+	t3.start();
+	t4.start();
+	//Thread.sleep(50000);
+	System.out.println("Main-Method-Completed");
     }
 }
 
-/*
-    1) extends the Thread class 
-    2) override the run method
-    3) create the objects 
-    4)
-*/
+
+
